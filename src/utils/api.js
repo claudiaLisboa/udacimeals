4b2c52fb6a5f43d77aac6412ab7f0e05a420211d
Claudia <ccalumby@gmail.com>
2017-09-14 23:54:34 -0400

    Food Modal

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 const API_ID = process.env.REACT_APP_API_ID
 const APP_KEY = process.env.REACT_APP_API_KEY
 
-export function fethRecipes (food = ''){
+export function fetchRecipes (food = ''){
     food = food.trim()
 
     return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
